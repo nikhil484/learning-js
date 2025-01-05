a579b8e026d64764e2c7e05cb2590d51317b7790
nikhil484 <86187347+nikhil484@users.noreply.github.com>
2025-01-05 21:03:23 +0000

    change

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId=12253
+let accountEmail="nikhilanand2202@gmail.com"
+var accountPassword="12345"
+accountCity="madhubani"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword])
